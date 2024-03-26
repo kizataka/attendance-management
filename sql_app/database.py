ea04b965433ae530202d0623f4ad4707e05bdf04
kizataka <chongxiongmu@gmail.com>
2024-03-26 10:32:07 +0900

    Changed SQLALCHEMY_DATABASE_URL

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'  # ローカル用
-SQLALCHEMY_DATABASE_URL = 'sqlite:////var/lib/data/sql_app.db'  # Render.comでの設定
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'  # ローカル用
+# SQLALCHEMY_DATABASE_URL = 'sqlite:////var/lib/data/sql_app.db'  # Render.comでの設定
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
